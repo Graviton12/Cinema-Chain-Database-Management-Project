d9c0dd92d806af015d8fa33dcd5b58793775d917
Samia Mahmood <122104724+Graviton12@users.noreply.github.com>
2025-11-27 20:47:17 -0500

    Add files via upload

---
+++ project1_SQL.sql
@@ -0,0 +1,236 @@
+DROP DATABASE IF EXISTS CINEMA_CHAIN;
+CREATE DATABASE CINEMA_CHAIN;
+USE CINEMA_CHAIN; #Default Database
+
+SET FOREIGN_KEY_CHECKS=0; #Disable ALL foreign key constraint checks
+
+CREATE TABLE CINEMA (
+	address VARCHAR(250) NOT NULL,
+    cinema_name VARCHAR (180) NOT NULL,
+    cinema_ID INT NOT NULL,
+    primary key (cinema_ID)
+);
+
+CREATE TABLE THEATER (
+	seating_capacity INT NOT NULL,
+    cinema_ID INT NOT NULL,
+    theater_number CHAR(3) NOT NULL,
+    primary key (theater_number),
+    foreign key (cinema_ID) REFERENCES CINEMA(cinema_ID)
+);
+
+CREATE TABLE MOVIE (
+	director_name VARCHAR(180) NOT NULL,
+    movie_name VARCHAR(250) NOT NULL,
+    time_slot TIME NOT NULL,
+    release_year YEAR NOT NULL,
+    movie_rating INT NOT NULL,
+    theater_num CHAR(3) NOT NULL,
+    primary key (movie_name, time_slot),
+    #unique(movie_name, release_year),
+    foreign key (theater_num) REFERENCES THEATER(theater_number)
+);
+
+CREATE TABLE PEOPLE (
+	bdate DATE NOT NULL,
+    age_group CHAR(5) NOT NULL,
+    awards_member CHAR(6),
+    email VARCHAR(255) NOT NULL,
+    primary key (email)
+);
+
+CREATE TABLE TICKET (
+	ticket_price DECIMAL(10,2) UNSIGNED NOT NULL,
+    movie_name VARCHAR(250) NOT NULL,
+    time_slot TIME NOT NULL,
+	email VARCHAR(255) NOT NULL,
+    ticket_price_index VARCHAR(3) NOT NULL,
+    primary key (ticket_price_index),
+    foreign key (movie_name, time_slot) REFERENCES MOVIE(movie_name, time_slot),
+    foreign key (email) REFERENCES PEOPLE(email)
+);
+
+CREATE TABLE WATCHED (
+	mov_name VARCHAR(250) NOT NULL,
+    time_slot TIME NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    num_watched INT NOT NULL,
+    primary key(mov_name, time_slot, email),
+    foreign key (mov_name, time_slot) REFERENCES MOVIE(movie_name, time_slot),
+    foreign key (email) REFERENCES PEOPLE(email)
+);
+
+# -- In order to enable the foreign keys, we need to insert values for each table -- #
+
+# insert values into CINEMA table
+INSERT INTO CINEMA 
+	VALUES
+('4400 Towne Center Dr, Louisville, KY 40241', 'Cinemark Tinseltown', 100),
+('12450 Sycamore Station Pl, Louisville, KY 40299', 'Xscape Theatres', 200),
+('2745 S Hurstbourne Pkwy, Louisville, KY 40220', 'AMC Stonybrook 20', 300),
+('2800 Gottbrath Pkwy, Jeffersonville, IN 47130', 'Xscape Jeffersonville 12', 400),
+('1250 Bardstown Rd, Louisville, KY 40204', 'Baxter Avenue Theatres', 500);
+
+# insert values into THEATER table
+INSERT INTO THEATER
+	VALUES
+(250, 100, 'A10'),
+(300, 200, 'B10'),
+(150, 300, 'C10'),
+(450, 400, 'D10'),
+(600, 500, 'E10');
+
+# insert values into MOVIE table
+INSERT INTO MOVIE
+	VALUES
+('Steven Spielberg', 'Jaws', '22:00:00', 1975, 5, 'A10'),
+('Christopher Nolan', 'Inception', '13:00:00', 2010, 4, 'B10'),
+('Steven Spielberg', 'Jurassic Park', '14:30:00', 1993, 5, 'C10'),
+('Colin Trevorrow', 'Jurassic World Dominion', '19:30:00', 2022, 2, 'D10'),
+('Adam Wingard', 'Godzilla x Kong: The New Empire', '12:00:00', 2024, 3, 'E10'),
+('Adam Wingard', 'Godzilla x Kong: The New Empire', '10:00:00', 2024, 3, 'F10'),
+('Greta Gerwig', 'Barbie', '23:00:00', 2023, 4, 'F10');
+
+# insert values into PEOPLE table
+INSERT INTO PEOPLE
+	VALUES
+('1998-04-12', 'adult', 'Golden', 'alex@gmail.com'),
+('2011-11-03', 'child', NULL,  'bella@gmail.com'),
+('1987-07-29', 'adult', 'Silver', 'carla@yahoo.com'),
+('1975-02-15', 'adult', 'Golden', 'derek@hotmail.com'),
+('2005-09-08', 'adult', NULL, 'emma@gmail.com'),
+('1999-10-05', 'adult', NULL, 'mary@gmail.com'),
+('2010-12-10', 'child', NULL,  'sarah710@gmail.com');
+
+# insert values into TICKET table
+INSERT INTO TICKET
+    VALUES
+(15.00, 'Jaws', '22:00:00', 'alex@gmail.com', 'T01'),             -- Alex, $15 per ticket
+(12.00, 'Jurassic World Dominion', '19:30:00', 'derek@hotmail.com', 'T02'), -- Derek, $12 per ticket
+(10.00, 'Jurassic Park', '14:30:00', 'carla@yahoo.com', 'T03'),   -- Carla, $10 per ticket
+(9.00, 'Inception', '13:00:00', 'bella@gmail.com', 'T04'),        -- Bella, $9 per ticket
+(8.00, 'Godzilla x Kong: The New Empire', '12:00:00', 'emma@gmail.com', 'T05'), -- Emma, $8 per ticket
+(7.00, 'Godzilla x Kong: The New Empire', '10:00:00', 'mary@gmail.com', 'T06'),
+(16.00, 'Barbie', '23:00:00', 'sarah710@gmail.com', 'T07'); -- Sarah, $16 per ticket
+
+# insert values into WATCHED table
+INSERT INTO WATCHED
+    VALUES
+('Jaws', '22:00:00', 'alex@gmail.com', 2),            -- Alex bought 2 tickets
+('Jurassic World Dominion', '19:30:00', 'derek@hotmail.com', 3), -- Derek bought 3 tickets
+('Jurassic Park', '14:30:00', 'carla@yahoo.com', 1),  -- Carla bought 1 ticket
+('Inception', '13:00:00', 'bella@gmail.com', 1),      -- Bella bought 1 ticket
+('Godzilla x Kong: The New Empire', '12:00:00', 'emma@gmail.com', 2), -- Emma bought 2 tickets
+('Godzilla x Kong: The New Empire', '10:00:00', 'mary@gmail.com', 1),
+('Barbie', '23:00:00', 'sarah710@gmail.com', 2); -- Sarah bought 2 tickets
+
+SET FOREIGN_KEY_CHECKS = 1; #We will now enable all the foreign key checks
+
+# -- Stored Procedure Queries -- #
+# Get total number of tickets sold for a specific movie for various show times (Stored Procedure with an Output Argument)
+DELIMITER //
+
+CREATE PROCEDURE total_tickets_sold(
+    IN in_movie_name VARCHAR(250),
+    OUT total_sold INT
+)
+BEGIN
+    SELECT SUM(num_watched) INTO total_sold
+    FROM WATCHED
+    WHERE mov_name = in_movie_name;
+END;
+//
+
+DELIMITER ;
+
+-- How many tickets were sold for the movie Jaws?
+CALL total_tickets_sold('Jaws', @sold);
+SELECT @sold;
+
+-- How many tickets were sold for the movie Jurassic World Dominion?
+CALL total_tickets_sold('Jurassic World Dominion', @sold);
+SELECT @sold;
+
+-- How many tickets were sold for the movie Godzilla x Kong: The New Empire?
+CALL total_tickets_sold('Godzilla x Kong: The New Empire', @sold);
+SELECT @sold;
+
+# Get the movie name with its release year and show time by a specific director (Stored Procedure with an Input Argument)
+DELIMITER //
+
+CREATE PROCEDURE movies_by_director(
+	IN in_director VARCHAR (180)
+)
+BEGIN
+	SELECT movie_name, release_year, time_slot
+    FROM MOVIE
+    WHERE director_name = in_director;
+END;
+//
+
+DELIMITER ;
+
+-- What movies with their associated release year and show time were directed by Steven Spielberg?
+CALL movies_by_director('Steven Spielberg');
+
+# For all ticket buyers, get their emails, age group, the movie name they watched, the show time, and the ticket price (Stored Procedure Involving a JOIN)
+SELECT PEOPLE.email, age_group, movie_name, time_slot, ticket_price
+FROM PEOPLE JOIN TICKET ON PEOPLE.email = TICKET.email;
+
+DELIMITER //
+
+CREATE PROCEDURE ticket_buyer_info()
+BEGIN
+	SELECT PEOPLE.email, age_group, movie_name, time_slot, ticket_price
+	FROM PEOPLE JOIN TICKET ON PEOPLE.email = TICKET.email;
+END;
+//
+
+DELIMITER ;
+
+-- what is the information for the all ticket buyers
+CALL ticket_buyer_info();
+
+# Get the number of movies scheduled per theater (Stored Procedure)
+DELIMITER //
+
+CREATE PROCEDURE movies_per_theater()
+BEGIN
+    SELECT theater_num, COUNT(*) AS num_movies
+    FROM MOVIE
+    GROUP BY theater_num;
+END;
+//
+
+DELIMITER ;
+
+CALL movies_per_theater()
+
+# Get the total number of tickets sold for each movie with its associated show times (Stored Procedure)
+DELIMITER //
+
+CREATE PROCEDURE movies_and_tickets_sold()
+BEGIN
+    SELECT MOVIE.movie_name, MOVIE.time_slot, SUM(WATCHED.num_watched) AS tickets_sold
+    FROM MOVIE JOIN WATCHED ON MOVIE.movie_name = WATCHED.mov_name AND MOVIE.time_slot = WATCHED.time_slot
+    GROUP BY MOVIE.movie_name, MOVIE.time_slot;
+END;
+//
+
+DELIMITER ;
+
+CALL movies_and_tickets_sold();
+
+# Get the total revenue generated by a movie  (Stored Prodecure)
+DELIMITER //
+
+CREATE PROCEDURE revenue_generated_per_movie()
+BEGIN
+	SELECT TICKET.movie_name, TICKET.time_slot, num_watched, num_watched*ticket_price AS revenue_generated
+	FROM TICKET JOIN WATCHED ON TICKET.movie_name = WATCHED.mov_name AND TICKET.time_slot = WATCHED.time_slot;
+END;
+//
+
+DELIMITER ;
+
+CALL revenue_generated_per_movie();
